94e206bed9953655c5519785c5d3d2507bc8da2f
MortCord <ratynsky98@gmail.com>
2022-12-04 15:28:36 +0200

    D incorrect

---
+++ D incorrect/src/MiniAx.java
@@ -0,0 +1,6 @@
+public class MiniAx {
+
+    boolean UsingAx(){
+        return true;
+    }
+}
---
+++ D incorrect/src/Robot.java
@@ -0,0 +1,7 @@
+public class Robot {
+    private boolean isPizzaDestroyed = false;
+    private boolean isKnifeBroken = true;
+    public Robot(){
+        System.out.println("I can eat pizza only with knife but my knife is broken");
+    }
+}
