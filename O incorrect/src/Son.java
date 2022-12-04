d00535e448665da313ac75e4830456d9029cb93b
MortCord <ratynsky98@gmail.com>
2022-12-04 14:57:56 +0200

    OO incorrect

---
+++ O incorrect/src/Son.java
@@ -0,0 +1,9 @@
+public class Son extends Father {
+    void playFootball(){
+        System.out.println("Son played with football");
+    }
+    void playCompGames(){
+        System.out.println("Son is playing comp games");
+    }
+
+}
